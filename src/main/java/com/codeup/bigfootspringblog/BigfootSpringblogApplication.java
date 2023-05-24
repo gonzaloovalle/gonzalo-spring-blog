982169f959ef3e661e83095750d74b16beb3e38b
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2023-05-24 09:48:59 -0500

    Initial commit

---
+++ src/main/java/com/codeup/bigfootspringblog/BigfootSpringblogApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.bigfootspringblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigfootSpringblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigfootSpringblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/bigfootspringblog/BigfootSpringblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.bigfootspringblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigfootSpringblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
